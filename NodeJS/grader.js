eafb9472c1c5d68b60094586c8e9f337b28484a5
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-11-02 17:09:53 -0700

    simple grade avg calc javascript

---
+++ NodeJS/grader.js
@@ -0,0 +1,18 @@
+function average(scores){
+    // add all scores together
+    var total = 0;
+    scores.forEach(function(score) {
+        total += score;
+    });
+
+    var avg = total/scores.length;
+    // divide by total number of scores
+
+    // round average
+    return Math.round(avg);
+}
+var scores = [ 90 , 98 , 89 , 100 , 100, 86, 94];
+console.log(average(scores)); // should return 94
+
+var scores2 = [ 40 , 65 , 77 , 82, 80 , 54 , 73, 63 , 95 , 49 ];
+console.log(average(scores2));  // should return 68
