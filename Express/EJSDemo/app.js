a2921e82d2a15c078d3293974ba21d0395b91f30
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-11-05 21:38:52 +0000

    passing arrays

--- Express/EJSDemo/app.js
+++ Express/EJSDemo/app.js
@@ -11,6 +11,18 @@ app.get("/love/:things", function(req, res)
    var things = req.params.things;
    res.render("love.ejs", {thingVar : things } )
 });
+
+app.get("/post", function(req, res)
+{
+   var posts = [
+       {title: "Can you believe this" , author: "Se Jin" }, 
+       {title: "What's up" , author: "Amy" }, 
+       {title: "Cool things" , author: "Charlie" }, 
+       ]; 
+       
+       res.render("post.ejs", {posts : posts}); 
+       
+});
 app.listen(process.env.PORT, process.env.IP, function()
 {
    console.log("The server is listening now .."); 
