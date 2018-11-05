432f037acc862adddc73b570b9093b99f2c4b840
Se Jin Lee <sjleesogang@gmail.com>
2018-11-05 07:43:17 +0000

    add default error page

--- Express/firstExpressApp/app.js
+++ Express/firstExpressApp/app.js
@@ -10,6 +10,19 @@ app.get("/", function(req, res)
 // "/bye" => "Good bye!"
 // "/dog"  => "MEOW!"
 //
+app.get("/Dog" , function(req, res) {
+	res.send("meow!");
+});
+
+app.get("/bye" , function(req,res) {
+	res.send("Goodbye!");
+});
+
+app.get("*", function(req, res){
+ 	res.send("You've entered a wrong URL.");
+});
+
+
 app.listen(process.env.PORT, process.env.IP, function()
 {
     console.log("Server has started");
