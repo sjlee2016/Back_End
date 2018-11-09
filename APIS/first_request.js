81f22d17f769d6e18da06f0b48d55febd210e923
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-11-09 18:12:13 +0000

    simple hhtp request

---
+++ APIS/first_request.js
@@ -0,0 +1,12 @@
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+    if(error){
+  console.log('something went wrong:', error);
+  }// Print the error if one occurred
+  else 
+  {
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+    }
+      
+});
