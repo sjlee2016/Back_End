5da1fefc7a6856eada1813465de5ca69ecb4ce45
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-11-09 18:32:55 +0000

    sunset js

--- APIS/first_request.js
+++ APIS/first_request.js
@@ -1,12 +1,9 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    if(error){
-  console.log('something went wrong:', error);
-  }// Print the error if one occurred
-  else 
-  {
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', function (error, response, body) {
+    if(!error && response.statusCode==200){
+        var parsedData = JSON.parse(body);
+        console.log("sunset in Hawaii is at .. ")
+        console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
     }
       
 });
