babe5c974db8a6009b21168031bf9bc8d7dcec6c
sjlee2016 <sjlee2016@sogang.ac.kr>
2018-11-10 00:30:25 -0800

    merge

--- Express/firstExpressApp/app.js
+++ Express/firstExpressApp/app.js
@@ -10,6 +10,16 @@ app.get("/", function(req, res)
 // "/bye" => "Good bye!"
 // "/dog"  => "MEOW!"
 //
+app.get("/bye", function(req, res) {
+    res.send("Good bye!");
+});
+
+
+app.get("/Dog", function(req,res) {
+    res.send("Meow!");
+});
+
+
 app.listen(3000, process.env.IP, function()
 {
     console.log("Server has started");
