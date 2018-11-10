04b8d6bdfcf887cf9c21977c6d0b810446d4d1e5
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-11-10 01:55:15 +0000

    Applied search page

--- APIS/MovieSearchApp/app.js
+++ APIS/MovieSearchApp/app.js
@@ -6,8 +6,14 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
 var APIKey = "e643f1c8"  // limited to 1000 searches per day 
-app.get("/", function(req, res){
-    request('http://www.omdbapi.com/?s=star&apikey='+ APIKey, function (error, response, body) {
+
+app.get("/search", function(req,res){
+   res.render("search"); 
+});
+
+app.get("/result", function(req, res){
+    
+    request('http://www.omdbapi.com/?s='+ req.query.title + '&apikey='+ APIKey, function (error, response, body) {
         
             var data = JSON.parse(body);
             //res.send(parsedData);
