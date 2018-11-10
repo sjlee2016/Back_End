3d48212b151a411a32a9ebcc65398b068e0f6fb6
Se Jin Lee <sjlee2016@sogang.ac.kr>
2018-11-10 01:46:15 +0000

    movie search app

---
+++ APIS/MovieSearchApp/app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+var APIKey = "e643f1c8"  // limited to 1000 searches per day 
+app.get("/", function(req, res){
+    request('http://www.omdbapi.com/?s=star&apikey='+ APIKey, function (error, response, body) {
+        
+            var data = JSON.parse(body);
+            //res.send(parsedData);
+            res.render("result", { data : data });
+})
+});
+
+app.listen(process.env.PORT , process.env.VIP, function()
+{
+   console.log("server has started.."); 
+});
