e0d224067e642ac80e0625d2b0d48d8f94a63e78
Se Jin Lee <sjlee2016@sogang.ac.kr>
2019-02-19 00:52:03 -0800

    first javascript project

---
+++ javascript/DOM-Manipulation/README.md
@@ -0,0 +1,16 @@
+# DOM
+
+## What is DOM?
+Document Object Model
+(Javascript and DOM are two different things.)
+* Structured representation of an HTML document, which is used to connect webpages to scripts like javascript.
+* For each HTML box, there is an object in the DOM that we can access and interact with.
+
+## HTML and CSS
+html : holds the content of the webpage
+css : presentation of the content
+
+## Events
+notification that are sent to notify the codew that something happened on the webpage
+like clicking a button.
+Event Listener : a function that performs an action based on certain event. 
---
+++ javascript/DOM-Manipulation/app.js
@@ -0,0 +1,82 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, gamePlaying; 
+initialize(); 
+function initialize(){
+    scores = [0,0]; 
+    roundScore = 0 ;
+    gamePlayer = true; 
+    activePlayer = 0;
+    gamePlaying = true;
+    document.getElementById('score-0').textContent = 0; 
+    document.getElementById('score-1').textContent = 0; 
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;  
+    document.querySelector('.dice').style.display = 'none'; 
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+};
+function changePlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+    roundScore=0; 
+    document.getElementById('current-0').textContent = '0'; 
+    document.getElementById('current-1').textContent = '0'; 
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none'; 
+}
+//document.querySelector("#current-" + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    if (!gamePlaying){
+       return; 
+    }
+    // get random number between 1 ~ 6 
+    var dice = Math.floor(Math.random() * 6 + 1);
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    if (dice !== 1){
+        // Add Score
+        roundScore += dice; 
+        document.querySelector('#current-' + activePlayer).textContent = roundScore; 
+    }else{
+        // next Player
+        changePlayer();
+    }
+}); 
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if (!gamePlaying){
+        return; 
+    }
+    // Add current score to global score
+    scores[activePlayer] += roundScore; 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    if (scores[activePlayer] >= 10 ){
+        document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'; 
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        document.querySelector('.dice').style.display = 'none';
+        gamePlaying=false;
+    }else{
+      changePlayer();
+    }
+});
+
+document.querySelector('.btn-new').addEventListener('click', initialize);
